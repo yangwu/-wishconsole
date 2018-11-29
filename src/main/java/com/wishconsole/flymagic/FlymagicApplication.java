32ee56c98b2a7845f178c71cca6b0c8ec4a31000
yangwu <yangwu@sanganvideo.com>
2018-11-29 11:11:01 +0800

    init from SpringBoot 2.1.0

---
+++ src/main/java/com/wishconsole/flymagic/FlymagicApplication.java
@@ -0,0 +1,14 @@
+package com.wishconsole.flymagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class FlymagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlymagicApplication.class, args);
+	}
+
+}
